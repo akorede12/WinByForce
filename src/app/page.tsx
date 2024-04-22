022f5432fbd2c45453c6e90f7fa1ea39a98854ba
VinceBrun <105801401+VinceBrun@users.noreply.github.com>
2024-04-22 01:19:15 +0100

    changed routing from pages to app router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import HeroButtons from "./components/herobuttons/HeroButton";
-import Browse from "./components/browse/Browse";
-import How from "./components/how/How";
-import Experience from "./components/experience/Experience";
+import HeroButtons from "@/components/herobuttons/HeroButton";
+import Browse from "@/components/browse/Browse";
+import How from "@/components/how/How";
+import Experience from "@/components/experience/Experience";
 
 export default function Home() {
   return (
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,43 @@
+export const Footer = () => {
+  return (
+    <div
+      className="px-[30px] md:px-[100px] py-[60px]
+      bg-[#0B0D0D] grid grid-cols-1 md:flex md:flex-row gap-4"
+    >
+      <div className="md:flex flex-wrap md:flex-row justify-between w-full">
+        <div className="md:flex justify-center items-start md:gap-[100px]">
+          <div className="flex flex-row gap-[100px]">
+            <div className="pt-[20px] sm:pt-0 flex flex-col gap-7">
+              <p className="text-[#BBBBBB] text-[15px] md:text-[20px] font-semibold">
+                Learn & Earn
+              </p>
+            </div>
+
+            <div className="pt-[20px] sm:pt-0 flex flex-col gap-7">
+              <p className="text-[#BBBBBB] text-[15px] md:text-[20px] font-semibold">
+                Protocols
+              </p>
+            </div>
+
+            <div className="pt-[20px] sm:pt-0 flex flex-col gap-7">
+              <p className="text-[#BBBBBB] text-[15px] md:text-[20px] font-semibold">
+                Community
+              </p>
+            </div>
+
+            <div className="pt-[20px] sm:pt-0 flex flex-col gap-7">
+              <p className="text-[#BBBBBB] text-[15px] md:text-[20px] font-semibold">
+                Terms Of Service
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+        <p className="text-[#BBBBBB] text-[15px] md:text-[20px] font-semibold">
+          BlocQuest 2024
+        </p>
+        </div>
+      </div>
+    </div>
+  );
+};
